c9fe80316bb8418dc3dcd5b59d8e6063727a13b4
杨阳 <yangyang@licaigc.com>
2018-12-11 17:42:15 +0800

    feature: add http client

---
+++ http/client.go
@@ -0,0 +1,173 @@
+package http
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+// Client A http client hold host and timeout
+type Client struct {
+	host    string
+	timeout time.Duration
+}
+
+// NewClient Create a http client with host, default timeout is 10s.
+func NewClient(host string) *Client {
+	return &Client{
+		host:    host,
+		timeout: 10 * time.Second,
+	}
+}
+
+// Request Send a request to target url with method and body.
+// The response body has been read out, while others are in resp.
+func Request(url, method string, body []byte) (*http.Response, []byte, error) {
+	client := NewClient(url)
+	client.SetTimeout(2 * time.Second)
+	req, err := client.BuildRequest("", method, body)
+	if err != nil {
+		return nil, nil, err
+	}
+	return client.Do(req)
+}
+
+// Request Send a request to client host and target path with method and body.
+// The response body has been read out, while others are in resp.
+func (this *Client) Request(path, method string, body []byte) (*http.Response, []byte, error) {
+	req, err := this.BuildRequest(path, method, body)
+	if err != nil {
+		return nil, nil, err
+	}
+	return this.Do(req)
+}
+
+// BuildRequest return a http.Request for any other expand.
+// Use http.NewRequest to create a request is worked, too.
+func (this *Client) BuildRequest(path, method string, body []byte) (*http.Request, error) {
+	var data io.Reader
+	if body != nil {
+		data = bytes.NewReader(body)
+	}
+	req, err := http.NewRequest(
+		method,
+		this.host+path,
+		data,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+
+// Do Send a request by using http.Request.
+// The response body has been read out to body, while others are in resp.
+func (this *Client) Do(req *http.Request) (resp *http.Response, body []byte, err error) {
+	client := &http.Client{
+		Timeout: this.timeout,
+	}
+	resp, err = client.Do(req)
+	if err != nil {
+		return resp, nil, err
+	}
+
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return resp, nil, err
+	}
+	return resp, body, nil
+}
+
+// SetTimeout Set the timeout value of request.
+func (this *Client) SetTimeout(d time.Duration) {
+	this.timeout = d
+}
+
+// KelpClient A http client for kelp server, extend Client.
+type KelpClient struct {
+	Client
+	token string
+}
+
+// NewKelpClient Create a KelpClient.
+// The param token will be put into http header Authorization, which server may required.
+// If the token is empty, there will no Authorization header.
+// Default timeout is 2s.
+func NewKelpClient(host, token string) *KelpClient {
+	client := &KelpClient{}
+	client.host = host
+	client.token = token
+	client.timeout = 2 * time.Second
+	return client
+}
+
+// RequestKelp Send a request to a kelp server. Default timeout is 2s.
+// The param token will be put into http header Authorization, which server may required.
+// If the token is empty, there will no Authorization header.
+// The param in is same as the handler param in, which define in the server.
+// The param out is a response data holder, must be a pointer.
+// The param lastContext is for request chain trace, such as traceid or uuid.
+// The response status is not nil when the server returns an error status.
+func RequestKelp(url, token string, in interface{}, out interface{}, lastContext *Context) (*Status, error) {
+	client := NewKelpClient(url, token)
+	client.SetTimeout(2 * time.Second)
+	return client.RequestKelp("", in, out, lastContext)
+}
+
+// RequestKelp Send a request to the kelp server with path by client created before.
+// The param in is same as the handler param in, which define in the server.
+// The param out is a response data holder, must be a pointer.
+// The param lastContext is for request chain trace, such as traceid or uuid.
+// The response status is not nil when the server returns an error status.
+func (this *KelpClient) RequestKelp(path string, in interface{}, out interface{}, lastContext *Context) (*Status, error) {
+	var body []byte
+	if in != nil {
+		body, _ = json.Marshal(in)
+	}
+	req, err := this.BuildRequest(path, "POST", body)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(this.token) > 0 {
+		req.Header.Set("Authorization", this.token)
+	}
+
+	if lastContext != nil {
+		req.Header.Set("Kelp-Traceid", lastContext.Request.Header.Get("Kelp-Traceid"))
+		req.Header.Set("uuid", lastContext.Request.Header.Get("uuid"))
+	}
+
+	_, responseBody, err := this.Do(req)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	type responseType struct {
+		Status  int             `json:"status"`
+		Message interface{}     `json:"message"`
+		Data    json.RawMessage `json:"data"`
+	}
+	response := &responseType{}
+	if err := json.Unmarshal(responseBody, response); err != nil {
+		return nil, err
+	}
+	if response.Status != 0 {
+		return JsonStatus(response.Status, response.Message), nil
+	}
+
+	if out == nil {
+		return nil, nil
+	}
+
+	raw, _ := response.Data.MarshalJSON()
+	if err := json.Unmarshal(raw, out); err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
--- http/doc/client.md
+++ http/doc/client.md
@@ -1,3 +1,61 @@
 http客户端
 ====
 
+直接发送请求
+----
+使用Request方法，直接发送请求：
+
+```
+response, body, err := http.Request("http://localhost/hello", "POST", []byte(`{"name":"jack"}`))
+```
+
+其中，response是标准包http的Response对象，它的Body已经被读取到返回值body中。
+
+
+构建服务对象
+----
+
+通过构建服务对象来发送请求，可以使服务的概念更清晰：
+
+```
+myService := http.NewClient("http://host")
+response, body, err := myService.Request("/hello", "POST", []byte(`{"name":"jack"}`))
+```
+
+扩展请求体
+----
+
+如有需要，可以对Request进行扩展：
+
+```
+myService := http.NewClient("http://host")
+req, err := myService.BuildRequest("/hello", "POST", []byte(`{"name":"jack"}`)
+
+// TODO check err
+req.Header.Set("My-Header", "value")
+response. body, err := myService.Do(req)
+
+```
+
+这里通过BuildRequest方法创建的Request是标准包http的Request对象，用户可以对其进行随意的修改定制。
+
+当然，用户也可以直接使用标准包http创建Request对象。
+
+请求kelp
+----
+
+在kelp/http包中，额外提供了直接请求通过kelp/http创建的http server(server创建参考[kelp/httpexample](/http/example/))的方法：
+
+```
+// use package method
+status, err := http.RequestKelp("http://host/hello", "my token if exist", in, out, lastContext)
+
+// use client service mode
+myService := http.NewKelpClient("http://host", "my token if exist")
+status, err := myService.RequestKelp("/hello", in, out, lastContext)
+```
+
+其中in和out按照Handler中定义的数据结构定义即可。
+
+当请求失败时，status和err都可能非空，注意分情况判断。
+如果status非空，那么它一定是server中定义的Status对象（这里要求Handler的返回值必须使用kelp/http包提供的Status的形式定义）。
